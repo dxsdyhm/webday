5b1a9e1150046702cb61c153c4b99d56daf45654
dxs_qmx_win <duanxuesong12@126.com>
2019-07-07 18:04:19 +0800

    预加入JSSDK的计划

--- qmorn/qmorn_vue/src/App.vue
+++ qmorn/qmorn_vue/src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+	import wx from 'weixin-js-sdk';
 	export default {
 		data() {
 			return {
@@ -27,10 +28,29 @@
 				this.$nextTick(function() {
 					this.isRouterAlive = true
 				})
+			},
+			wxConfig() {
+				this.axios({
+					method: 'post',
+					url: 'http://my.service.com/index.php/opcode/6002',
+					data: {
+						url: location.href.split('#')[0]
+					} //向服务端提供授权url参数，并且不需要#后面的部分
+				}).then((res) => {
+					wx.config({
+						debug: true, // 开启调试模式,
+						appId: res.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
+						timestamp: res.timestamp, // 必填，生成签名的时间戳
+						nonceStr: res.nonceStr, // 必填，生成签名的随机串
+						signature: res.signature, // 必填，签名，见附录1
+						jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+					});
+				})
 			}
 		},
 		mounted: function() {
 			this.onResize()
+			//this.wxConfig() //暂时没有后台支持
 		},
 		provide: function() {
 			return {
